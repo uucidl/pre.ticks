03f36ff9cb3b680ddfd905536d97b8b9230cdbe4
Nicolas <nicolas@uucidl.com>
2014-11-18 10:21:49 +0100

    instruction on how to paste shadertoy shaders

--- src/draw-on-quad-with-shader/shader.fs
+++ src/draw-on-quad-with-shader/shader.fs
@@ -2,11 +2,19 @@
 
 uniform vec3 iResolution; // viewport resolution in pixels
 uniform float iGlobalTime; // shader playback time in seconds
-out vec4 color;
+out vec4 oColor;
+
+/*
+  this is somewhat[1] compatible with shadertoy shaders:
+  - paste it below,
+  - replace gl_FragColor with oColor
+
+  [1] we do not support texture channels or mouse input right now
+*/
 
 void main()
 {
         vec2 uv = gl_FragCoord.xy/iResolution.xy;
         float g = uv.y * (1.0f + 0.5 * sin(8.0*3.141592*(iGlobalTime/4.0 + uv.x)));
-        color = vec4(uv.x, g, uv.y, 1.00);
+        oColor = vec4(uv.x, g, uv.y, 1.00);
 }
